c36c89b2447a08a85e1cf49fca19e008f6c7c883
pawanmann <pawanmann89@gmail.com>
2021-12-21 09:55:46 +0530

    one more file added

--- Datatypes.py
+++ Datatypes.py
@@ -1,3 +1,18 @@
 message1="i love python 3"
 message2="Hi!!"
+message3="Happy Friendship day"
 print(message2+" Pawan")
+print(message2,message1)
+a="2"
+print(a*5)
+b=3
+print(b*5)
+
+# %d for int
+# %f for float
+# %s for string
+
+print("wishing you %s %s" % (message3 ,message1))
+
+print(a+ message1)
+print(b,message1)
